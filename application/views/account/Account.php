18e0a8c2610e02ae5c0a99076be9098a1fa77435
gtpesantren22 <cakromat26@gmailcom>
2023-02-20 19:43:05 +0700

    tambah pencairan peminjaman di account

--- application/views/account/Account.php
+++ application/views/account/Account.php
@@ -1372,8 +1372,8 @@ https://simkupaduka.ppdwk.com/';
 		$data['lembaga'] = $this->model->getBy2('lembaga', 'kode', $this->lembaga, 'tahun', $this->tahun)->row();
 		$data['pjnData'] = $this->model->getBy2('pengajuan', 'tahun', $this->tahun, 'verval', 0);
 
+		$data['cicil'] = $this->model->getBy('cicilan', 'tahun', $this->tahun)->result();
 		$data['dataPinjam'] = $this->model->getBy('peminjaman', 'tahun', $this->tahun, 'id_pinjam', $id)->row();
-		$data['cicil'] = $this->model->getBy('cicilan', 'kode_pinjam', $data['dataPinjam']->kode_pinjam)->result();
 		$data['sumPinjam'] = $this->model->getBySum('peminjaman', 'tahun', $this->tahun, 'nominal')->row();
 
 		$data['sumCicil'] = $this->model->getBySum('cicilan', 'kode_pinjam', $data['dataPinjam']->kode_pinjam, 'nominal')->row();
