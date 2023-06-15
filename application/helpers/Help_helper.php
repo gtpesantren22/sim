16b7059f1ec258bc68f9d5e934fcd76d1d951656
ACCDW <106668739+gtpesantren22@users.noreply.github.com>
2023-06-15 20:57:40 +0700

    Update Help_helper.php
    Ganti alamat whapi

--- application/helpers/Help_helper.php
+++ application/helpers/Help_helper.php
@@ -71,7 +71,7 @@ function kirim_person($key, $no_hp, $pesan)
     curl_setopt_array(
         $curl2,
         array(
-            CURLOPT_URL => 'http://8.215.26.187:3000/api/sendMessage',
+            CURLOPT_URL => 'http://156.67.221.155:3000/api/sendMessage',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -92,7 +92,7 @@ function kirim_group($key, $id_group, $pesan)
     curl_setopt_array(
         $curl2,
         array(
-            CURLOPT_URL => 'http://8.215.26.187:3000/api/sendMessageGroup',
+            CURLOPT_URL => 'http://156.67.221.155:3000/api/sendMessageGroup',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -113,7 +113,7 @@ function kirim_tmp($key, $no_hp, $pesan, $tmp, $link_logo)
     curl_setopt_array(
         $curl2,
         array(
-            CURLOPT_URL => 'http://8.215.26.187:3000/api/sendTemplateMessage',
+            CURLOPT_URL => 'http://156.67.221.155:3000/api/sendTemplateMessage',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
